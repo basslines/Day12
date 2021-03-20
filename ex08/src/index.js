1e57f34ffcaafe9246ab153e30b0f8a8a9065ff6
basslines <admir.osmanovic@gmail.com>
2021-03-20 01:20:27 +0100

    dodao index.js fajl

---
+++ ex08/src/index.js
@@ -0,0 +1,38 @@
+var contacts = [
+  {
+    firstName: "John",
+    lastName: "Doe",
+    number: "12365495208",
+    likes: ["C", "C++", "C#"],
+  },
+  {
+    firstName: "Jane",
+    lastName: "Doe",
+    number: "456829761",
+    likes: ["Python", "Swift", "R"],
+  },
+  {
+    firstName: "Richard",
+    lastName: "Roe",
+    number: "6938257149",
+    likes: ["React", "Angular", "Vue"],
+  },
+  {
+    firstName: "Jane",
+    lastName: "Roe",
+    number: "unknown",
+    likes: ["JavaScript", "Node", "HTML & CSS"],
+  },
+];
+
+function lookUpProfile(name, prop) {
+  
+}
+
+console.log(lookUpProfile("John", "likes"));
+console.log(lookUpProfile("Jane", "lastName"));
+console.log(lookUpProfile("Richard", "likes"));
+console.log(lookUpProfile("Rob", "number"));
+console.log(lookUpProfile("Rob", "JavaScript"));
+console.log(lookUpProfile("John", "address"));
+module.exports = lookUpProfile;
